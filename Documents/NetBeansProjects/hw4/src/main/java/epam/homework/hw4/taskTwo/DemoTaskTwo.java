b589b2f5f1a7b79e8a55f4d93f3d462f0ceb37b7
lex-Under <fenr1r16n@yandex.ru>
2017-10-27 16:04:04 +0400

    - Инжектор по SRP и без статической инициализации - Добавлен класс CacheGetter для получения нужного инстанса, заполняемого CacheFiller - Author.equals() отвязан от равенства хэш-кодов - DemoTastTwo.bookByAuthors() возвращает теперь Мапу автор-СписокКниг, вместро строк - прочие причёсы кода

--- Documents/NetBeansProjects/hw4/src/main/java/epam/homework/hw4/taskTwo/DemoTaskTwo.java
+++ Documents/NetBeansProjects/hw4/src/main/java/epam/homework/hw4/taskTwo/DemoTaskTwo.java
@@ -36,9 +36,10 @@ public class DemoTaskTwo {
     public List<Author> pensioners(List<Author> authors) {
         return authors.stream()
                 .filter(a -> {
-                    return (a.getGender() == Author.Gender.male)
-                            ? !(a.isDead()) && (a.getAge() > 65)
-                            : !(a.isDead()) && (a.getAge() > 63);
+                    return ((!a.isDead())
+                            && ((a.getGender() == Author.Gender.MALE)
+                                    ? (a.getAge() > 65)
+                                    : (a.getAge() > 63)));
                 })
                 .collect(Collectors.toList());
     }
@@ -57,42 +58,40 @@ public class DemoTaskTwo {
                 .collect(Collectors.toList());
     }
 
-    public Map<String, List<String>> booksByAuthors(List<Author> authors, List<Book> books) {
+    public Map<Author, List<Book>> booksByAuthors(List<Author> authors, List<Book> books) {
         return books.stream()
                 .flatMap(book -> book.getAuthors().stream()
                         .map(author -> new SimpleEntry<>(author, book)))
                 .collect(Collectors.groupingBy(
-                        e -> e.getKey().getName(),
-                        Collectors.mapping(e -> e.getValue().getName(), Collectors.toList())));
+                        e -> e.getKey(),
+                        Collectors.mapping(e -> e.getValue(), Collectors.toList())));
     }
 
     public void launch() {
-
         init();
         System.out.println("\nСписок всех авторов:");
-        authors.stream().forEach(System.out::println);
+        authors.forEach(System.out::println);
         System.out.println("\nСписок всех книг:");
-        books.stream().forEach(System.out::println);
+        books.forEach(System.out::println);
         System.out.println("\nСредний возраст авторов: " + averageAge(authors));
         System.out.println("\nСписок авторов, отсортированный по возрастанию возраста:");
-        sortByAge(authors).stream().forEach(System.out::println);
+        sortByAge(authors).forEach(System.out::println);
         System.out.println("\nАвторы-пенсионеры:");
-        pensioners(authors).stream().forEach(System.out::println);
+        pensioners(authors).forEach(System.out::println);
         System.out.println("\nКнига --- возраст книги:");
-        namesAndAges(books).stream().forEach(System.out::println);
+        namesAndAges(books).forEach(System.out::println);
         System.out.println("\nСписок всех авторов, писавших в соавторстве с кем-либо:");
-        coauthors(books).stream().forEach(System.out::println);
+        coauthors(books).forEach(System.out::println);
         System.out.println("\nАвтор --- книги автора:");
-        booksByAuthors(authors, books).entrySet().stream().forEach(System.out::println);
-
+        booksByAuthors(authors, books).entrySet().forEach(System.out::println);
     }
 
     private void init() {
-        Author strugatskyA = new Author("Аркадий Натанович Стругацкий", LocalDate.of(1925, 8, 28), LocalDate.of(1991, 10, 12), Author.Gender.male);
-        Author strugatskyB = new Author("Борис Натанович Стругацкий", LocalDate.of(1933, 4, 15), LocalDate.of(2012, 11, 9), Author.Gender.male);
-        Author efremov = new Author("Иван Антонович Ефремов", LocalDate.of(1908, 4, 22), LocalDate.of(1972, 10, 5), Author.Gender.male);
-        Author gitkovich = new Author("Вадим Константинович Гиткович", LocalDate.of(1924, 7, 6), LocalDate.of(1977, 10, 28), Author.Gender.male);
-        Author pualo = new Author("Пуало Коэльо", LocalDate.of(1947, 8, 24), Author.Gender.male);
+        Author strugatskyA = new Author("Аркадий Натанович Стругацкий", LocalDate.of(1925, 8, 28), LocalDate.of(1991, 10, 12), Author.Gender.MALE);
+        Author strugatskyB = new Author("Борис Натанович Стругацкий", LocalDate.of(1933, 4, 15), LocalDate.of(2012, 11, 9), Author.Gender.MALE);
+        Author efremov = new Author("Иван Антонович Ефремов", LocalDate.of(1908, 4, 22), LocalDate.of(1972, 10, 5), Author.Gender.MALE);
+        Author gitkovich = new Author("Вадим Константинович Гиткович", LocalDate.of(1924, 7, 6), LocalDate.of(1977, 10, 28), Author.Gender.MALE);
+        Author pualo = new Author("Пуало Коэльо", LocalDate.of(1947, 8, 24), Author.Gender.MALE);
         Book nichegoNet = new Book("Ничего нет прекрасней Земли...", 1968, Arrays.asList(strugatskyA, efremov));
         Book lezvie = new Book("Лезвие бритвы", 1964, Arrays.asList(efremov));
         Book prognoz = new Book("Прогноз", 1989, Arrays.asList(strugatskyA, strugatskyB));
